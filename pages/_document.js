14156882c6e7eb625d6b8eb7f21be2b61cc0624b
Simogithub2023 <143028222+Simogithub2023@users.noreply.github.com>
2023-08-27 18:08:09 +0100

    Update _document.js

--- pages/_document.js
+++ pages/_document.js
@@ -24,6 +24,17 @@ class MyDocument extends Document {
             
             {/* Add the rest of your SEO text here */}
           </div>
+            <!-- Google tag (gtag.js) -->
+<script async src="https://www.googletagmanager.com/gtag/js?id=G-P6VRL9DFK9"></script>
+<script>
+  window.dataLayer = window.dataLayer || [];
+  function gtag(){dataLayer.push(arguments);}
+  gtag('js', new Date());
+
+  gtag('config', 'G-P6VRL9DFK9');
+</script>
+  <script async src="https://pagead2.googlesyndication.com/pagead/js/adsbygoogle.js?client=ca-pub-2690887042898420"
+     crossorigin="anonymous"></script>
         </body>
       </Html>
     );
